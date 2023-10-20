27d43bbd8a08d94d9f210ea3b4b6c10ecb852909
Shweta Shivgunde <122803080+SS2508@users.noreply.github.com>
2023-10-20 15:07:02 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hii , Welcome to Java programming ");
+		System.out.print("datatype defines the type and behavior of the data .");
+		System.out.print("Common Datatypes");
+		System.out.println("premative and nonprimative datatype");
+	}
+	
+}
